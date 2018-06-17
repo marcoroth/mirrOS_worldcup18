91c580c7a56a02d02d530403c553002725ffa680
Marco Roth <marco.roth@intergga.ch>
2018-06-17 14:29:26 +0200

    add messages for no games today and confiugure in settings

--- frontend/script.js
+++ frontend/script.js
@@ -1,12 +1,14 @@
 var worldcup18_url;
+var worldcup18_apikey;
 
 $(document).ready(function() {
 
 	worldcup18_url = "https://api.football-data.org/v1/competitions/467/fixtures";
+	worldcup18_apikey = "<?php echo getConfigValue('worldcup18_apikey'); ?>";
 
 	$.ajaxSetup({
 	  headers : {
-	    "X-Auth-Token": "<?php echo getConfigValue('worldcup18_apikey'); ?>"
+	    "X-Auth-Token": worldcup18_apikey
 	  },
 	  async: false
 	});
@@ -15,55 +17,67 @@ $(document).ready(function() {
 });
 
 function worldcup18(){
-	$.getJSON(worldcup18_url).done(function(data){
-			today = new Date();
-			today_string = today.getDate().pad(2) + "." + (today.getMonth()+1).pad(2) + "." + today.getFullYear();
-			$("#worldcup18_table").empty();
 
-	    $(data.fixtures).each(function(index, fixture) {
-	      date = new Date(fixture.date);
+	if (worldcup18_apikey != "") {
+		$.getJSON(worldcup18_url).done(function(data){
+				today = new Date();
+				today_string = today.getDate().pad(2) + "." + (today.getMonth()+1).pad(2) + "." + today.getFullYear();
+				$("#worldcup18_table").empty();
 
-				$("#worldcup18_date").text(today_string);
+		    $(data.fixtures).each(function(index, fixture) {
+		      date = new Date(fixture.date);
 
-	      if (date.getMonth() == today.getMonth() && date.getDate() == today.getDate()){
-	        $.getJSON(fixture._links.awayTeam.href).done(function(awayTeam){
-	          awayTeamFlagUrl = awayTeam.crestUrl;
+					$("#worldcup18_date").text(today_string);
 
-	          $.getJSON(fixture._links.homeTeam.href).done(function(homeTeam){
-	            $("#worldcup18_table").append("<tr></tr>");
-	            $("#worldcup18_table tr:last").append("<td>" + date.getHours().pad(2) + ":" + date.getMinutes().pad(2) + "<td>");
+		      if (date.getMonth() == today.getMonth() && date.getDate() == today.getDate()){
+		        $.getJSON(fixture._links.awayTeam.href).done(function(awayTeam){
+		          awayTeamFlagUrl = awayTeam.crestUrl;
 
-	            if (fixture.status == "IN_PLAY"){
-	              $("#worldcup18_table tr:last").append("<td><i class='fa fa-circle' style='color: red'></i><td>");
-	            }
+		          $.getJSON(fixture._links.homeTeam.href).done(function(homeTeam){
+		            $("#worldcup18_table").append("<tr></tr>");
+		            $("#worldcup18_table tr:last").append("<td>" + date.getHours().pad(2) + ":" + date.getMinutes().pad(2) + "<td>");
 
-	            if (fixture.status == "FINISHED"){
-	              $("#worldcup18_table tr:last").append("<td><i class='fa fa-check'></i><td>");
-	            }
+		            if (fixture.status == "IN_PLAY"){
+		              $("#worldcup18_table tr:last").append("<td><i class='fa fa-circle' style='color: red'></i><td>");
+		            }
 
-	            if (fixture.status == "TIMED"){
-	              $("#worldcup18_table tr:last").append("<td><i class='fa fa-clock-o'></i><td>");
-	            }
+		            if (fixture.status == "FINISHED"){
+		              $("#worldcup18_table tr:last").append("<td><i class='fa fa-check'></i><td>");
+		            }
 
-	            $("#worldcup18_table tr:last").append("<td> <img width='20px' src='" + homeTeam.crestUrl + "'> " + fixture.homeTeamName + "</td>");
+		            if (fixture.status == "TIMED"){
+		              $("#worldcup18_table tr:last").append("<td><i class='fa fa-clock-o'></i><td>");
+		            }
 
-	            if (fixture.status == "FINISHED" || fixture.status == "IN_PLAY") {
-	              $("#worldcup18_table tr:last").append("<td>" + fixture.result.goalsHomeTeam + "<td>");
-	              $("#worldcup18_table tr:last").append("<td>-<td>");
-	              $("#worldcup18_table tr:last").append("<td>" + fixture.result.goalsAwayTeam + "<td>");
-	            } else {
-	              $("#worldcup18_table tr:last").append("<td>?<td>");
-	              $("#worldcup18_table tr:last").append("<td>-<td>");
-	              $("#worldcup18_table tr:last").append("<td>?<td>");
-	            }
+		            $("#worldcup18_table tr:last").append("<td> <img width='20px' src='" + homeTeam.crestUrl + "'> " + fixture.homeTeamName + "</td>");
 
-	            $("#worldcup18_table tr:last").append("<td> <img width='20px' src='" + awayTeam.crestUrl + "'> " + fixture.awayTeamName + "</td>");
+		            if (fixture.status == "FINISHED" || fixture.status == "IN_PLAY") {
+		              $("#worldcup18_table tr:last").append("<td>" + fixture.result.goalsHomeTeam + "<td>");
+		              $("#worldcup18_table tr:last").append("<td>-<td>");
+		              $("#worldcup18_table tr:last").append("<td>" + fixture.result.goalsAwayTeam + "<td>");
+		            } else {
+		              $("#worldcup18_table tr:last").append("<td>?<td>");
+		              $("#worldcup18_table tr:last").append("<td>-<td>");
+		              $("#worldcup18_table tr:last").append("<td>?<td>");
+		            }
 
-	          });
-	        });
-	      }
-	    });
-	  });
+		            $("#worldcup18_table tr:last").append("<td><img width='20px' src='" + awayTeam.crestUrl + "'> " + fixture.awayTeamName + "</td>");
+		          });
+		        });
+		      }
+		    });
+
+				if ($("#worldcup18_table tr").size() == 0) {
+					$("#worldcup18_table").append("<tr></tr>");
+					$("#worldcup18_table tr:last").append("<td><i class='fa fa-2x fa-futbol-o'></i><td>");
+					$("#worldcup18_table tr:last").append("<td><?php echo _('worldcup18worldcup18_no_games_today'); ?><td>");
+				}
+
+		  });
+		} else {
+			$("#worldcup18_table").append("<tr></tr>");
+			$("#worldcup18_table").append("<td><?php echo _('worldcup18_configure_in_settings'); ?><td>");
+		}
 
 
 	window.setTimeout(function() {
